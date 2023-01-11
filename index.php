179f10ef333f68cab669a1ae8201f44309960ee3
Damien <damien.deboudt@sfr.fr>
2023-01-11 13:34:26 +0100

    ok

--- index.php
+++ index.php
@@ -2,20 +2,26 @@
 
 // Premiere ligne
 
-{
+function maFonction () {
     echo "Hello World!";
 }
 
 //Deuxieme ligne
 
-
+maFonction();
 
 //Troisieme ligne
-
+/**
+ * @param $fname string  name
+ * @param $lname string  surname
+ * @return mixed
+ */
 function maFonctionParam($fname, $lname) {
-    echo ;
+    echo $fname;
+    return $lname;
 }
 
+maFonctionParam("joe", "cray");
 
 
 
